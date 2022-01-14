8521452561b1f1a9960de0b7ab68dad165298655
Mike Mulshine <mrmulshine@gmail.com>
2022-01-14 12:40:09 -0500

    A little restructuring (tending to flatten hierarchy).

--- tutorial/build.sh
+++ tutorial/build.sh
@@ -1,8 +1,8 @@
-pandoc index.md -o index.html
-pandoc ./help/local-server.md -o local-server.html
-pandoc ./tutorials/001.md -o tutorial-01.html
-pandoc ./tutorials/002.md -o tutorial-02.html
-pandoc ./tutorials/003.md -o tutorial-03.html
+pandoc ./md/index.md -o index.html
+pandoc ./md/local-server.md -o local-server.html
+pandoc ./md/001.md -o tutorial-01.html
+pandoc ./md/002.md -o tutorial-02.html
+pandoc ./md/003.md -o tutorial-03.html
 osascript -e 'tell application "Google Chrome" to tell the active tab of its first window to reload'
 #open -a "Google Chrome" http://localhost:$1/
 open -a "Google Chrome"
