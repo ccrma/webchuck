77aae92af5343920f40fb2b923c91453b1bb9d6e
terry feng <tzfeng@ucsd.edu>
2023-02-04 21:10:34 -0800

    add working examples folder structure and json reading

--- js/examples.js
+++ js/examples.js
@@ -1,120 +1,269 @@
-// Load ChucK file from URL
-const examplesURL = "https://raw.githubusercontent.com/ccrma/chuck/main/";
-const subfolders = ["examples", "basic"];
+// Load ChucK examples from URL
+const EXAMPLES_ROOT_URL = "https://raw.githubusercontent.com/ccrma/chuck/main/";
+let folderPath = ["examples"];
+let moreExamplesJSON;
 var exampleCode = "";
 var exampleName = "";
+
+// Load the more exemples JSON file
+readJSONExamples().then(() =>
+{
+    renderFolderFiles();
+});
 
 // More Examples button in Nav Bar
 var moreExamplesButton = document.getElementById("moreExamplesButton");
 moreExamplesButton.addEventListener("click", function () { window.location.hash = "more-examples"; });
 
-/* LOADING EXAMPLE VIA URL */
-// Step 1: Fetch ChucK Example into Preview Window
-var loadExampleFromURL = function (url)
+//------------------------------------------------
+// EXAMPLES FILE BROWSER 
+// Handle loading of files
+//------------------------------------------------
+/**
+ * Create a file element to display a subfolder or chuck example
+ */
+function createFileElement(fileName)
+{
+    // Create a file element
+    var file = document.createElement("a");
+    file.classList.add("btn", "btn-link");
+
+    // If file is a chuck example
+    if (fileName.split('.').pop() === "ck")
+    {
+        file.addEventListener("onclick", loadExampleFromURL(createURL(fileName)));
+        file.innerHTML = file.id = fileName;
+    } else
+    {
+        file.id = fileName;
+        // Make folders bold
+        file.innerHTML = "<b>" + fileName + "</b>";
+        file.classList.add("font-weight-bold");
+        file.addEventListener("click", function ()
+        {
+            // Update the path to the current folder
+            folderPath.push(fileName);
+            // Render breadcrumbs after update
+            renderBreadcrumbs();
+            // Render folder files after update
+            renderFolderFiles();
+        });
+    }
+
+    return file;
+}
+
+/**
+ * Render all the contents of a folder directory to the screen
+ * This is called when the folder path is updated 
+ */
+var exampleFolder = document.getElementById("example-folder");
+function renderFolderFiles() 
+{
+    // reset folder contents
+    exampleFolder.innerHTML = "";
+
+    // Read in all the files in the folder from the JSON file
+    var folderContents = retrieveFolderContents();
+    // Split the folders and files into two lists, if .ck extension, it is a file
+    var folders = folderContents["folders"];
+    var files = folderContents["files"];
+    // Alphabetize both lists
+    folders.sort();
+    files.sort();
+
+    // Render folders list, then render files list
+    for (let i = 0; i < folders.length; i++)
+    {
+        exampleFolder.appendChild(createFileElement(folders[i]));
+    }
+    for (let i = 0; i < files.length; i++)
+    {
+        exampleFolder.appendChild(createFileElement(files[i]));
+    }
+};
+
+//---------------------------------------------------------------------
+// EXAMPLES PREVIEW WINDOW FUNCTIONS
+// Data functions for examples preview window
+//---------------------------------------------------------------------
+/**
+ * Load ChucK file from URL, this loads into the preview window
+ * @param {String} url 
+ */
+function loadExampleFromURL(url)
 {
     // Check if URL leads to a .ck file
-    if (url.split('.').pop() !== "ck") {
+    if (url.split('.').pop() !== "ck")
+    {
         console.log("Error: URL does not lead to a .ck file");
         return;
     }
 
     // Fetch ChucK file from URL
-    fetch(url).then(res => res.text()).then(text =>
-    {
-        exampleCode = text;
-        exampleCodePreview.innerHTML = text;
-        exampleName = url.split("/").pop();
-    });
+    fetch(url)
+        .then(handleErrors)
+        .then(res => res.text()).then(text =>
+        {
+            exampleCode = text;
+            exampleCodePreview.innerHTML = text;
+            exampleName = url.split("/").pop();
+        })
+        .catch(error =>
+        {
+            console.log("Error fetching file: ", error)
+            exampleCodePreview.innerHTML = "Error fetching " + url + ": " + error;
+        });
 };
-// Step 2: Load Preview Window Code into Editor
-var loadChuckExample = function ()
+
+/**
+ * Load preview window code into the editor
+ * Sets the global file name to the example name
+ */
+var loadPreviewToEditor = function ()
 {
+    if (exampleCode === "")
+    {
+        return;
+    }
     loadChuckFileFromString(exampleCode);
     localStorage['chuckCacheName'] = globalFileName = exampleName;
     printToOutputConsole("Loaded chuck example: " + exampleName);
 };
-// Preview Code Example Load Button
+
+// Get example code preview window
 var exampleCodePreview = document.getElementById("example-code");
+// Load example button
 var loadPreviewButton = document.getElementById("loadPreviewButton");
 loadPreviewButton.addEventListener("click", function ()
 {
-    loadChuckExample();
+    loadPreviewToEditor();
     window.location.hash = "#";
 });
 
-// Create url with file name
-var createURL = function (fileName)
-{
-    return examplesURL + subfolders.join("/") + "/" + fileName;
-};
-
-/*------------------------------------------------*/
-/* EXAMPLES FILE BROWSER */
-/*------------------------------------------------*/
+//---------------------------------------------------------------------
+// BREADCRUMB NAVIGATION FUNCTIONS
+// Breadcrumb navigation and management
+//---------------------------------------------------------------------
 // Breadcrumb Navigation renderer from subfolder list
-var examplesBreadcrumb = document.getElementById("examples-breadcrumb");
-var renderBreadcrumbs = function ()
+var breadcrumbBar = document.getElementById("examples-breadcrumb");
+
+/**
+ * Create a breadcrumb element
+ * Handles the onclick event and can update the folder path 
+ * @param {int} index 
+ */
+function createBreadcrumb(folderName, index)
+{
+    // Create breadcrumb
+    var breadcrumb = document.createElement("li");
+    breadcrumb.className = "breadcrumb-item";
+    // Add link to breadcrumb
+    var breadcrumbButton = document.createElement("button");
+    breadcrumbButton.textContent = folderName;
+    breadcrumbButton.onclick = function ()
+    {
+        // Update the path to the current breadcrumb folder
+        cutPath(index);
+        // Render breadcrumbs after update
+        renderBreadcrumbs();
+        // Render folder files after update
+        renderFolderFiles();
+    };
+    breadcrumb.appendChild(breadcrumbButton);
+
+    return breadcrumb;
+}
+
+/**
+ * Populate breadcrumb navigation bar to the modal
+ */
+function renderBreadcrumbs()
 {
     // Clear breadcrumbs
-    examplesBreadcrumb.innerHTML = "";
+    breadcrumbBar.innerHTML = "";
 
-    // Create folder breadcrumbs
-    for (let i = 0; i < subfolders.length; i++) {
-        var breadcrumb = document.createElement("li");
-        breadcrumb.className = "breadcrumb-item";
-        // Add link to breadcrumb
-        var breadcrumbLink = document.createElement("a");
-        breadcrumbLink.href = ""; // TODO: LINKS DON'T WORK YET
-        breadcrumbLink.innerHTML = subfolders[i];
-        breadcrumb.appendChild(breadcrumbLink);
-        // Add breadcrumb to breadcrumb list
-        examplesBreadcrumb.appendChild(breadcrumb);
-        examplesBreadcrumb.appendChild(document.createTextNode("\u00A0"));
+    // Create breadcrumb trail for folders
+    for (let i = 0; i < folderPath.length; i++)
+    {
+        breadcrumbBar.appendChild(createBreadcrumb(folderPath[i], i));
+        // note: add this whitespace char cause weird spacing 
+        breadcrumbBar.appendChild(document.createTextNode("\u00A0"));
     }
 };
 renderBreadcrumbs();
 
-// TESTING ONLY
-// TODO: temporary way to load files...need to implement a file browser
-// only basic examples, I hate this rn
-basic = ['adc.ck', 'comb.ck', 'envelope.ck',
-    'infnan.ck', 'rec.ck', 'whole.ck',
-    'adsr.ck', 'curly++.ck', 'fm.ck',
-    'larry++.ck', 'ring.ck', 'wind.ck',
-    'alarm.ck', 'curly.ck', 'fm2.ck',
-    'larry.ck', 'sndbuf.ck', 'wind2.ck',
-    'args.ck', 'delay.ck', 'fm3.ck',
-    'lfo.ck', 'step.ck', 'zerox.ck',
-    'bar.ck', 'demo0.ck', 'foo.ck',
-    'moe++.ck', 'tick.ck', 'blit.ck',
-    'demo1.ck', 'foo2.ck', 'moe.ck',
-    'tick2.ck', 'blit2.ck', 'demo2.ck',
-    'func.ck', 'oscillatronx.ck', 'unchuck.ck',
-    'chirp.ck', 'demo3.ck', 'i-robot.ck',
-    'rec-auto-stereo.ck', 'valueat.ck', 'chirp2.ck',
-    'echo.ck', 'imp.ck', 'rec-auto.ck',
-    'whirl.ck'];
-
-var exampleFolder = document.getElementById("example-folder");
-var renderFolderFiles = function ()
+//---------------------------------------------------------------------
+// PATH MANAGEMENT HELPER FUNCTIONS
+// Retrieve file/file paths, path list management, url to path
+//---------------------------------------------------------------------
+/**
+ * Handle errors in fetch requests 
+ * @param {*} response 
+ * @returns response if response is ok
+ */
+function handleErrors(response)
 {
-    // Clear folder
-    exampleFolder.innerHTML = "";
-
-    // Render folder files from basic list
-    for (let i = 0; i < basic.length; i++) {
-        var file = document.createElement("a");
-        file.classList.add("btn");
-        file.classList.add("btn-link");
-        file.id = basic[i];
-        file.innerHTML = basic[i];
-
-        file.addEventListener("click", function ()
-        {
-            loadExampleFromURL(createURL(this.id));
-        });
-
-        exampleFolder.appendChild(file);
+    if (!response.ok)
+    {
+        throw Error(response.statusText);
     }
-};
-renderFolderFiles();
+    return response;
+}
+
+/**
+ * Read in JSON file of example file paths 
+ */
+function readJSONExamples()
+{
+    // return a promise
+    return fetch('./examples/moreExamples.json')
+        .then(handleErrors)
+        .then(response => response.json())
+        .then(data => moreExamplesJSON = data);
+}
+
+
+/**
+ * Use path list to retrieve list of files/folders from JSON object 
+ * @returns {Array} List of files/folders in current subfolder
+ */
+function retrieveFolderContents()
+{
+    // Lookup the contents of the last folder in the path
+    let lookup = folderPath[folderPath.length - 1];
+    let folderContents = moreExamplesJSON[lookup];
+
+    // Split folders and files (.ck) into two lists
+    let files = folderContents.filter(file => file.split('.').pop() === "ck");
+    let folders = folderContents.filter(file => !files.includes(file));
+
+    return { "folders": folders, "files": files };
+}
+
+/**
+ * Append subfolder to path list 
+ * @param {*} subfolder 
+ */
+function appendToPath(subfolder)
+{
+    folderPath.push(subfolder);
+}
+
+/**
+ * Split path list at index, remove all subfolders after index
+ * @param {int} index 
+ */
+function cutPath(index)
+{
+    folderPath = folderPath.slice(0, index + 1);
+}
+
+/**
+ * Create URL to a chuck example 
+ * @returns {String} URL to a chuck example
+ */
+function createURL(fileName)
+{
+    return EXAMPLES_ROOT_URL + folderPath.join("/") + "/" + fileName;
+}
