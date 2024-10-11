d1841cbe2d4f16c2e43b331cb3d4e149f734a97e
terry feng <tzfeng@ucsd.edu>
2024-10-11 15:52:59 -0700

    add webchuck version and improve landing page UI

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+//-----------------------------------------------------------------------------
+// Version Updater
+//-----------------------------------------------------------------------------
+// fetch the version from package.json and update the version number in the footer
+fetch('./package.json').then(response => response.json()).then(data => {
+  const version = data.version;
+  const versionElement = document.getElementById('version-id');
+  versionElement.textContent = version;
+  versionElement.href = `https://github.com/ccrma/webchuck/releases/tag/v` + version;
+});
+
+//-----------------------------------------------------------------------------
+// WebChucK TRY ME!
+//-----------------------------------------------------------------------------
 var code;
 fetch("blit2.ck").then((response) => response.text()).then((text) => {
     code = text;
@@ -47,7 +61,7 @@ document.getElementById("try").addEventListener("click", function() {
   if (play) {   
     animationContainer.textContent = "";
     theChuck.removeLastCode();
-    document.getElementById("try").textContent = "Try WebChucK!";
+    document.getElementById("try").textContent = "Try Me!";
     stopTypeWriterAnimation();
   } else {
     typeWriterAnimation(animationContainer, code, delay);
@@ -57,4 +71,55 @@ document.getElementById("try").addEventListener("click", function() {
   play = !play;
 });
 
+//-----------------------------------------------------------------------------
+// Build WebChucK Bundle Zipper
+//-----------------------------------------------------------------------------
+const downloadZipButton = document.getElementById('downloadZipButton');
+
+async function downloadSrc(event) {
+  event.preventDefault();
+  downloadZipButton.innerText = 'Downloading...';
+
+  const zip = new JSZip();
+
+  const fileUrls = [
+    './src/wc-bundle.js',
+    './src/webchuck.js',
+    './src/webchuck.wasm'
+  ];
+
+  // Fetch each file and add it to the zip
+  for (const url of fileUrls) {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch ${url}`);
+    }
+    if (url.endsWith(".js")) {
+        const content = await response.text();
+        zip.file(url.split('/').pop(), content);
+    } else {
+        const content = await response.arrayBuffer();
+        zip.file(url.split('/').pop(), content, { binary: true });
+    }
+  }
+
+  // Generate the zip file and trigger the download
+  zip.generateAsync({ type: 'blob' }).then(function(content) {
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(content);
+    a.download = 'webchuck.zip';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }).catch(function(err) {
+    console.error('Failed to generate zip', err);
+  }).then(function() {
+    downloadZipButton.innerText = 'Download';
+  });
+}
+
+document.getElementById('downloadZip').addEventListener('click', downloadSrc);
+downloadZipButton.addEventListener('click', downloadSrc);
+
+/* MISC. */
 hljs.highlightAll();
